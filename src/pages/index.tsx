100c2b6f6c2b50c568f51e50f2cd06a77b08f15a
Michael Olie <molie@rarecollectiblestv.com>
2023-04-26 14:30:27 -0700

    added input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import axios from "axios";
 
 const pageStyles = {
   color: "#232129",
@@ -9,6 +10,21 @@ const pageStyles = {
 import "../styles/main.css";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [email, setEmail] = React.useState<string>("");
+  const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+
+  const handleSubmit = async (): Promise<void> => {
+    const res = await axios.post("https://allesgoldapi.afam.app/subscribers", {
+      email,
+    });
+    if (res.status !== 200) {
+      console.log("Error");
+      return;
+    }
+    console.log("Success");
+  };
+
   return (
     <main id="page__container" style={pageStyles}>
       <section className="page__overlay">
@@ -21,6 +37,16 @@ const IndexPage: React.FC<PageProps> = () => {
             Be the first to know when we launch, so you can receive professional
             guidance on receiving a world class catering experience!
           </p>
+          <span className="input">
+            <input
+              onChange={handleEmailInput}
+              value={email}
+              type="text"
+              placeholder="Subscribe to newsletter!"
+            />
+            <span></span>
+          </span>
+          {email.length > 2 && <button onClick={handleSubmit}>Submit</button>}
         </article>
       </section>
     </main>
